ff5e97f194da447dea021a0a0aa727089dc9e4a9
Michal vel m@lbit <michal@arqma.com>
2018-11-15 05:57:41 +0000

    wrong paths fixes

--- install-ios.sh
+++ install-ios.sh
@@ -8,7 +8,7 @@ OPEN_SSL_URL="https://github.com/x2on/OpenSSL-for-iPhone.git"
 OPEN_SSL_DIR_PATH="$EXTERNAL_DIR_PATH/OpenSSL"
 ARQMA_GUI_URL="https://github.com/malbit/arqma-gui.git"
 ARQMA_GUI_DIR_PATH="$EXTERNAL_DIR_PATH/arqma-gui"
-ARQMA_URL="https://github.com/arqma/arqma.git"
+#ARQMA_URL="https://github.com/arqma/arqma.git"
 ARQMA_DIR_PATH="$ARQMA_GUI_DIR_PATH/arqma"
 
 echo "Init external libs."
@@ -31,13 +31,13 @@ cd $OPEN_SSL_DIR_PATH
 ./build-libssl.sh --version=1.0.2j
 cd $SOURCE_DIR
 
-echo "============================ Monero-gui ============================"
+echo "============================ Arqma-gui ============================"
 
 echo "Cloning arqma-gui from - $ARQMA_GUI_URL"
 git clone -b build $ARQMA_GUI_URL $ARQMA_GUI_DIR_PATH
 cd $ARQMA_GUI_DIR_PATH
-echo "Cloning Arqma from - $ARQMA_URL to - $ARQMA_DIR_PATH"
-git clone -b build $ARQMA_URL $ARQMA_DIR_PATH
+#echo "Cloning Arqma from - $ARQMA_URL to - $ARQMA_DIR_PATH"
+#git clone -b build $ARQMA_URL $ARQMA_DIR_PATH
 echo "Export Boost vars"
 export BOOST_LIBRARYDIR="`pwd`/../ofxiOSBoost/build/ios/prefix/lib"
 export BOOST_INCLUDEDIR="`pwd`/../ofxiOSBoost/build/ios/prefix/include"
