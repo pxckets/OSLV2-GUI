8fb15e37182718ce0d047bfceb5507d374d463d9
m@lbit <malbit@arqma.com>
2019-05-15 23:58:16 +0100

    Arqma GUI v0.4 pre-release

--- src/libwalletqt/WalletManager.h
+++ src/libwalletqt/WalletManager.h
@@ -147,8 +147,8 @@ public:
     Q_INVOKABLE bool parse_uri(const QString &uri, QString &address, QString &payment_id, uint64_t &amount, QString &tx_description, QString &recipient_name, QVector<QString> &unknown_parameters, QString &error) const;
     Q_INVOKABLE QVariantMap parse_uri_to_object(const QString &uri) const;
     Q_INVOKABLE bool saveQrCode(const QString &, const QString &) const;
-    Q_INVOKABLE void checkUpdatesAsync(const QString &software, const QString &subdir) const;
-    Q_INVOKABLE QString checkUpdates(const QString &software, const QString &subdir) const;
+    Q_INVOKABLE void checkUpdatesAsync(const QString &software) const;
+    Q_INVOKABLE QString checkUpdates(const QString &software) const;
 
     // clear/rename wallet cache
     Q_INVOKABLE bool clearWalletCache(const QString &fileName) const;
